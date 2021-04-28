d6c71ada7da8cd5028cbae15c731852ffdba4def
Aragao96 <aragao96.rafael@gmail.com>
2021-04-28 05:30:33 -0400

    atualizado

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ soldado_direito, soldado_esquerdo = 100,124
 
 space_width, space_heigth = 100,124
 
-soldado1 = pygame.image.load(os.path.join('/home/karenrafael/Documentos/jogo_labirinto/sordadin.jpg'))
+soldado1 = pygame.image.load(os.path.join('/home/karenrafael/Documentos/fps_de_soldados/jogo-de-tiro-com-soldado/sordadin.jpg'))
 sordado1 = pygame.transform.scale(soldado1, (space_width, space_heigth))
-soldado2 = pygame.image.load(os.path.join('/home/karenrafael/Documentos/jogo_labirinto/sordadin2.png'))
+soldado2 = pygame.image.load(os.path.join('/home/karenrafael/Documentos/fps_de_soldados/jogo-de-tiro-com-soldado/sordadin2.png'))
 sordado2 = pygame.transform.scale(soldado2, (space_width,space_heigth))
 
 
