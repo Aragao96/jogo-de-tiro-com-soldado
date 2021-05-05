61b6eacf8f18921e421a68a12950152dc0223602
Aragao96 <aragao96.rafael@gmail.com>
2021-05-05 18:13:57 -0400

    atualizado

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 import os
 
-
-width, heigth = 900,500
-janela = pygame.display.set_mode((width, heigth))
+'''configurações do game, janela, fps, cor de fundo, velocidade'''
+width, height = 900,500
+janela = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Meu jogo')
 white = (255,255,255)
+black = (255,255,255)
 space_width, space_heigth = 100,124
+borda = pygame.Rect(width/2 - 5, 0, 10,height)
 fps = 60
 vel = 5
 
@@ -18,33 +20,34 @@ sordado2 = pygame.transform.scale(soldado2, (space_width,space_heigth))
 
 def draw_display(sold1, sold2):
     janela.fill(white)
+    pygame.draw.rect(janela, black, borda)
     janela.blit(sordado1, (sold1.x, sold1.y))
     janela.blit(sordado2, (sold2.x, sold2.y))        
     pygame.display.update()
 
 def soldado1_movimento(keys_pressed, sold1):
-    if keys_pressed[pygame.K_s]: #pra baixo 
+    if keys_pressed[pygame.K_s] and sold1.y + vel + sold1.height < height - 15: #pra baixo 
         sold1.y += vel
-    if keys_pressed[pygame.K_w]: #pra cima
+    if keys_pressed[pygame.K_w] and sold1.y - vel > 0: #pra cima
         sold1.y -= vel
-    if keys_pressed[pygame.K_a]: #pra esquerda 
+    if keys_pressed[pygame.K_a] and sold1.x - vel > 0: #pra esquerda 
         sold1.x -= vel 
-    if keys_pressed[pygame.K_d]: #pra direita
+    if keys_pressed[pygame.K_d] and sold1.x + vel + sold1.width < borda.x: #pra direita
         sold1.x += vel
 
 def soldado2_movimento(keys_pressed, sold2):
-    if keys_pressed[pygame.K_DOWN]: #pra baixo 
+    if keys_pressed[pygame.K_DOWN] and sold2.y + vel + sold2.height < height: #pra baixo 
         sold2.y += vel
-    if keys_pressed[pygame.K_UP]: #pra cima
-        sold2.y -= vel
-    if keys_pressed[pygame.K_LEFT]: #pra esquerda 
+    if keys_pressed[pygame.K_UP] and sold2.y - vel > 0: #pra cima
+        sold2.y -= vel 
+    if keys_pressed[pygame.K_LEFT] and sold2.x - vel > borda.x + borda.width: #pra esquerda 
         sold2.x -= vel 
-    if keys_pressed[pygame.K_RIGHT]: #pra direita
+    if keys_pressed[pygame.K_RIGHT] and sold2.x + vel + sold2.width < width: #pra direita
         sold2.x += vel 
 
 def main():
     sold1 = pygame.Rect(0,0, space_width, space_heigth)
-    sold2 = pygame.Rect(810,0, space_width, space_heigth)  
+    sold2 = pygame.Rect(805,2, space_width, space_heigth)  
     clock = pygame.time.Clock()
     run = True
     while run:
@@ -57,8 +60,6 @@ def main():
         soldado2_movimento(keys_pressed, sold2)
         draw_display(sold1, sold2)
         #draw_display()
-    pygame.quit()
-
 
 if __name__ == "__main__":
     main()
